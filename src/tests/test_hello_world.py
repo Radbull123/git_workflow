dc106de50b09b4161da1e601626a148b68fb1290
kobzar91 <taras.dmyterko@levi9.com>
2022-10-25 13:34:57 +0300

    Create pipline

---
+++ src/tests/test_hello_world.py
@@ -0,0 +1,9 @@
+import pytest
+from src.hello_world import hello_user, hello_world
+
+
+def test_hello_user():
+    assert hello_user() == 'Hello User'
+    
+def test_hello_world():
+    assert hello_world() == "Hello World"
